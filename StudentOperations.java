034e519513be55c77ea49f529f125d390d8ecc10
daniel john jacob <danieljj1805@gmail.com>
2025-03-04 17:34:56 +0530

    added studentoperation addstudent

---
+++ StudentOperations.java
@@ -0,0 +1,22 @@
+// StudentOperations.java
+
+class StudentOperations {
+    private final ArrayList<Student> students = new ArrayList<>();
+    private final Scanner sc = new Scanner(System.in);
+
+    public void addStudent() {
+        System.out.print("Enter PRN: ");
+        String prn = sc.nextLine();
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter DoB: ");
+        String dob = sc.nextLine();
+        System.out.print("Enter Marks: ");
+        double marks = sc.nextDouble();
+        sc.nextLine();
+        students.add(new Student(prn, name, dob, marks));
+        System.out.println("Student Added Successfully!");
+    }
+
+    
+}
